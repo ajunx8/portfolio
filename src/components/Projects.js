5072f878e655813e22853f7b35ee36a900cf9470
Adrian Greksa <adrian.greksa@gmail.com>
2022-09-16 20:31:57 +1000

    removed tabs in projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Container, Row, Tab, Nav } from 'react-bootstrap'
+import { Col, Container, Row} from 'react-bootstrap'
 import { ProjectCard } from './ProjectCard'
 import ProjectData from './ProjectData'
 import 'animate.css'
@@ -12,70 +12,18 @@ export const Projects = () => {
           <Col>
             <h2>Projects</h2>
             <p>I've worked on a variety of projects both individually & in collaboration with other developers.</p>
-            <Tab.Container id='projects-tabs' defaultActiveKey='first'>
-              <Nav variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Solo</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Group</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Latest</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                <Tab.Pane eventKey='first'>
-                  <Row>
-                    {
-                      ProjectData.map((project, index) => {
-                        if (project.type === 'Solo' || project.type2 === 'Solo') {
-                          return (
-                            <ProjectCard
-                              key={index}
-                              {...project}
-                            />
-                          )
-                        }
-                      })
-                    }
-                  </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey='second'>
-                  <Row>
-                    {
-                      ProjectData.map((project, index) => {
-                        if (project.type === 'Group') {
-                          return (
-                            <ProjectCard
-                              key={index}
-                              {...project}
-                            />
-                          )
-                        }
-                      })
-                    }
-                  </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey='third'>
-                  <Row>
-                    {
-                      ProjectData.map((project, index) => {
-                        if (project.type === 'Latest') {
-                          return (
-                            <ProjectCard
-                              key={index}
-                              {...project}
-                            />
-                          )
-                        }
-                      })
-                    }
-                  </Row>
-                </Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
-
+            <Row>
+              {
+                ProjectData.map((project, index) => {
+                  return (
+                    <ProjectCard
+                      key={index}
+                      {...project}
+                    />
+                  )
+                })
+              }
+            </Row>
           </Col>
         </Row>
       </Container>
