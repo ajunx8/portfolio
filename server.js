ae6b1bf72216bf70feff17f4bd88beaedd0e78a5
Adrian Greksa <adrian.greksa@gmail.com>
2022-11-02 12:40:29 +1100

    added gmail oauth2

--- server.js
+++ server.js
@@ -3,41 +3,59 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const path= require("path");
+
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 // server used to send emails
 const app = express();
-const port = process.env.PORT || 3001
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'))
-  app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
-}
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const port = process.env.PORT || 3001;
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(port, () => console.log(`Server Running On Port ${port}`));
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-});
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+              reject();
+          }
+          resolve(token);
+      });
+  });
+
+  const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+          type: "OAuth2",
+          user: process.env.EMAIL,
+          accessToken,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN
+      }
+  });
+
+  return transporter;
+};
 
 router.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
+
   const mail = {
     from: email,
     to: "adrian.greksa@gmail.com",
@@ -45,17 +63,47 @@ router.post("/contact", (req, res) => {
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
-           <p>${message}</p>`,
+           <p>Message: ${message}</p>`,
   };
-
-  console.log("Attempting to send email", mail);
   
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200 });
-    }
-  });
+  const sendEmail = async (emailOptions) => {
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(emailOptions, (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json({ code: 200 });
+      }
+    });
+  }
+
+  sendEmail(mail);
+
+  // contactEmail.sendMail(mail, (error) => {
+  //   if (error) {
+  //     res.json(error);
+  //   } else {
+  //     res.json({ code: 200 });
+  //   }
+  // });
 });
 
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.EMAIL,
+//     accessToken,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     refreshToken: process.env.REFRESH_TOKEN
+//   },
+// });
+
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,23 +24,26 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     setButtonText('Sending...')
-    fetch(`${process.env.REACT_APP_API_URL}/contact`, {
+    let response = await fetch(`http://localhost:3001/contact`, {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
       body: JSON.stringify(formDetails)
-    })
-    .then(res => res.json())
-    .then(results => {
-      setButtonText('Send')
-      setFormDetails(formInitalDetails)
-      if (results.code === 200) {
-        setStatus({ success: true, message: "Message Sent!" })
-      }  else {
-        setStatus({ success: false, message: 'Something went wrong, please try again later' })
-      }
-    })
-    
-  }
+    });
+
+    // .then(res => res.json())
+    // .then(results => {
+
+    setButtonText('Send');
+    let result = await response.json();
+    setFormDetails(formInitalDetails);
+    if (result.code === 200) {
+      setStatus({ success: true, message: "Message sent successfully" })
+    } else {
+      setStatus({ success: false, message: 'Something went wrong, please try again later' })
+    }
+
+    // })
+  };
 
   return (
     <section className="contact" id="contact">
@@ -51,24 +54,24 @@ export const Contact = () => {
             <form onSubmit={handleSubmit}>
               <Row>
                 <Col sm={6} className='px-1'>
-                  <input type="text" value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)}/>
+                  <input type="text" value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="text" value={formDetails.lastName} placeholder='Last Name' onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                  <input type="text" value={formDetails.lastName} placeholder='Last Name' onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="email" value={formDetails.email} placeholder='Email Address' onChange={(e) => onFormUpdate('email', e.target.value)}/>
+                  <input type="email" value={formDetails.email} placeholder='Email Address' onChange={(e) => onFormUpdate('email', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="telephone" value={formDetails.phone} placeholder='Phone Number' onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                  <input type="telephone" value={formDetails.phone} placeholder='Phone Number' onChange={(e) => onFormUpdate('phone', e.target.value)} />
                 </Col>
                 <Col className='px-1'>
                   <textarea rows="6" value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                   {
-                  status.message &&
-                  <Col>
-                    <p className='light contact-msg'>{status.message}</p>
-                  </Col>
+                    status.message &&
+                    <Col>
+                      <p className='light contact-msg'>{status.message}</p>
+                    </Col>
                   }
                   <button type='submit'><span>{buttonText}</span></button>
                 </Col>
